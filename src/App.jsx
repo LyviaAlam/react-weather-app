4b0165e9bed19ce5268e99f715f8e225ffc93058
LyviaAlam <154422742+LyviaAlam@users.noreply.github.com>
2025-05-21 13:40:55 +1000

    Installed Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
     <>
       <div>
         <h1>Hello</h1>
+        <button class="btn btn-primary">Try button</button>
+        <h1>
+          Example heading <span class="badge text-bg-secondary">New</span>
+        </h1>
         <Footer />
       </div>
     </>
